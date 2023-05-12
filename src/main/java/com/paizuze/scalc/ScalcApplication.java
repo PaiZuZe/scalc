052a58e0b36d6a2b5a3f48b3e4f17008cccfb998
Victor Chiaradia Gramuglia <victorcga96@gmail.com>
2023-05-12 11:42:16 -0300

    Spring initializr boiler plate

---
+++ src/main/java/com/paizuze/scalc/ScalcApplication.java
@@ -0,0 +1,13 @@
+package com.paizuze.scalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paizuze/scalc/ScalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paizuze.scalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
