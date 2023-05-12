e4209cf6ce2cb64bd1b7808215ed1c2e75fad26a
Victor Chiaradia Gramuglia <victorcga96@gmail.com>
2023-05-12 16:37:34 -0300

    Adds lombok and Thymeleaf dependecies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "3.0.6"
 	id("io.spring.dependency-management") version "1.1.0"
+	id("io.freefair.lombok") version "8.0.1"
 }
 
 group = "com.paizuze"
@@ -14,6 +15,8 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
